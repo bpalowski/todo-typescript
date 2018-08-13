b74cf721473dd6eb0584cc6ac4be1ad879e8449e
brian palowski and hanleyjames <brian@>
2018-08-13 08:40:13 -0700

    Doggo is maxed out

--- app/to-do1.js
+++ app/to-do1.js
@@ -4,6 +4,9 @@ var Task = /** @class */ (function () {
         this.priorityParameter = priorityParameter;
         this.done = false;
     }
+    Task.prototype.markDone = function () {
+        this.done = true;
+    };
     return Task;
 }());
 var tasks = [];
@@ -13,5 +16,11 @@ tasks.push(new Task('Feed the dog.', 'Medium'));
 tasks.push(new Task('Take dog outside.', 'High'));
 for (var _i = 0, tasks_1 = tasks; _i < tasks_1.length; _i++) {
     var task = tasks_1[_i];
+    console.log(task);
+}
+console.log("break before marking done");
+tasks[0].markDone();
+for (var _a = 0, tasks_2 = tasks; _a < tasks_2.length; _a++) {
+    var task = tasks_2[_a];
     console.log(task);
 }
--- app/to-do1.ts
+++ app/to-do1.ts
@@ -1,13 +1,22 @@
 class Task {
   done: boolean = false;
   constructor(public descriptionParameter: string, public priorityParameter: string){}
+  markDone(){
+    this.done = true;
+  }
 }
 
-var tasks: Task[] = [];
+let tasks: Task[] = [];
 tasks.push(new Task('Pet the dog.', 'High'));
 tasks.push(new Task('Hug the dog.', 'Low'));
 tasks.push(new Task('Feed the dog.', 'Medium'));
 tasks.push(new Task('Take dog outside.', 'High'));
+
 for(let task of tasks){
   console.log(task);
+}
+console.log("break before marking done");
+tasks[0].markDone();
+for(let task of tasks){
+  console.log(task)
 }
